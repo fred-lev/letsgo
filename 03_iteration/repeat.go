791a5db09eda5c158fd0050ea9d8a46f4fca240e
fredflev <frederic.leveque@r3.com>
2021-06-05 19:50:59 +0800

    update repeat function

--- 03_iteration/repeat.go
+++ 03_iteration/repeat.go
@@ -1,10 +1,9 @@
 package iteration
 
-const repeatCount = 5
-
-func Repeat(character string) string {
+// Repeat returns character repeated `repeatCount` times.
+func Repeat(character string, repeatCount int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
 	return repeated
