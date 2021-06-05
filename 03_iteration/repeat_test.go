95b4d8f1a85782b1d5e7408c3e4e3276e8b940d6
fredflev <frederic.leveque@r3.com>
2021-06-05 19:31:48 +0800

    add for loop

--- 03_iteration/repeat_test.go
+++ 03_iteration/repeat_test.go
@@ -1,1 +1,12 @@
 package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expeceted %q but got %q", expected, repeated)
+	}
+}
